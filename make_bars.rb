4db3ddce92e966835b7f4d937348351d55fed745
Russell Sherman <russell.t.sherman@gmail.com>
2016-03-18 19:00:28 -0500

    refactor

--- make_bars.rb
+++ make_bars.rb
@@ -6,18 +6,30 @@ require 'barby/outputter/prawn_outputter'
 
 include Prawn::Measurements #for in2pt methods
 
-numtix = {
-  adult: 2850,
-  teen: 75,
-  kid: 50,
-  child: 25
+# define tickets numbers, prices, and code prefix
+tix = {
+  adult: {
+    code: 'A',
+    count: 2850,
+    price: 111
+  },
+  teen: {
+    code: 'T',
+    count: 75,
+    price: 111
+  },
+  kid: {
+    code: 'K',
+    count: 50,
+    price: 33
+  },
+  child: {
+    code: 'C',
+    count: 25,
+    price: 0
+  }
 }
-prices = {
-  adult: 111,
-  teen: 111,
-  kid: 33,
-  child: 0
-}
+
 ticket_height = in2pt(4.88) # height in inches
 ticket_width = in2pt(2.13) # width in inches
 
@@ -31,45 +43,50 @@ pdf_opts = {
 pdf = Prawn::Document.new(pdf_opts)
 pdf.font 'Helvetica'
 
-a_tnums = Array (1..numtix[:adult]).collect{ |n| sprintf 'A%04d', n}
-t_tnums = Array (1..numtix[:teen]).collect{ |n| sprintf 'T%04d', n}
-k_tnums = Array (1..numtix[:kid]).collect{ |n| sprintf 'K%04d', n}
-c_tnums = Array (1..numtix[:child]).collect{ |n| sprintf 'C%04d', n}
-ticketnums = a_tnums + t_tnums + k_tnums + c_tnums
-
-ticketnums.each do |number|
-  secret = SecureRandom.hex(3).upcase
-  case number[0,1]
-  when "A"
-    price = prices[:adult]
-  when "T"
-    price = prices[:teen]
-  when "K"
-    price = prices[:kid]
-  when "C"
-    price = prices[:child]
+# generate hash of ticket data
+tickets = {}
+tix.each do |k, v|
+  tickets[k.to_sym]=[];
+  (1..v[:count]).each do |n|
+    tickets[k.to_sym][n] = {
+      number: (sprintf "#{v[:code]}%04d", n),
+      code: SecureRandom.hex(3).upcase,
+      price: v[:price]
+    }
   end
+end
 
-  barcode = Barby::Code128.new("#{number} #{secret}")
-  bc_out = Barby::PrawnOutputter.new(barcode)
+# iterate ticket data and generate pdf document
+tickets.each do |type, block|
 
-  bc_out.height = in2pt(0.2)
+  puts "Generating #{type.capitalize} tickets"
 
-  # if changing ticket height or width you may also want to modify the barcode placement
-  bc_place = {
-    :x => 10,
-    :y => 21,
-    :xdim => 0.8,
-    :margin => 5
-  }
-  bc_out.annotate_pdf(pdf, bc_place )
+  block.each do |ticket|
+    next if ticket.nil?
+    puts ticket
 
-  codez_txt.write("#{number} #{secret}\n")
+    bc = Barby::Code128.new("#{ticket[:number]} #{ticket[:code]}")
+    bc_out = Barby::PrawnOutputter.new(bc)
+    bc_out.height = in2pt(0.2)
 
-  pdf.text_box "#{number} #{secret}", :at => [10,61], :size=>9, :character_spacing=>1, :align=>:center
-  pdf.text_box "#{number} #{secret} $#{price}", :at => [10,19], :size=>9, :character_spacing=>1, :align=>:center
+    # if changing ticket height or width you may also want to modify the barcode placement
+    bc_place = {
+      :x => 10,
+      :y => 21,
+      :xdim => 0.8,
+      :margin => 5
+    }
+    bc_out.annotate_pdf(pdf, bc_place )
 
-  pdf.start_new_page unless number == ticketnums.last
+    codez_txt.write("#{ticket[:number]} #{ticket[:code]}\n")
+
+    pdf.text_box "#{ticket[:number]} #{ticket[:code]}",
+      :at => [10,61], :size=>9, :character_spacing=>1, :align=>:center
+    pdf.text_box "#{ticket[:number]} #{ticket[:code]} $#{ticket[:price]}",
+      :at => [10,19], :size=>9, :character_spacing=>1, :align=>:center
+
+    pdf.start_new_page
+  end
 end
 
 codez_txt.close
